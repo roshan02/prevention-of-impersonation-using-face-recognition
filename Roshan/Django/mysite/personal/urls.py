796877c889d7f5a916370fb597c10372c703c365
roshan02 <dn.roshan2@gmail.com>
2018-03-19 21:37:34 +0530

    temp files added

---
+++ Roshan/Django/mysite/personal/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from . import views
+from django.urls import path
+
+urlpatterns = [
+#    path('$', views.index, name = 'index')
+    url(r'^$', views.index, name = 'index'),
+    url(r'^contact/', views.contact, name= 'contact')	
+]
---
+++ Roshan/Django/mysite/personal/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+	return render(request, "personal/home.html")
+
+def contact(request):
+	return render(request, "personal/basic.html", {'content': ['If you would like to contact me just email me', 'dn.roshan2@gmail.com']})
+
+# Create your views here.
