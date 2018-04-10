f6aa90ae1b27ec1abe3c63a18c8839c4a7de8c86
AanchalChugh <aanchalchugh4@gmail.com>
2018-04-10 22:20:58 +0530

    Add files via upload

---
+++ Aanchal Chugh/personal/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django import forms
+
+def index(request):
+	class NameForm(forms.Form):
+		your_name = forms.ImageField(label='snapshot', max_length=100)
+		print(your_name)
+	return render(request, "personal/home.html")
+
+def contact(request):
+	return render(request, "personal/basic.html", {'content': ['If you would like to contact me just email me', 'dn.roshan2@gmail.com']})
+
+def success(request):
+	return render(request, "personal/login_success.html")
+
